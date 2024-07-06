2b77727a79fecec404216f5daab07807d06a8301
ignacio212121 <143823515+ignacio212121@users.noreply.github.com>
2024-07-06 14:29:23 -0300

    Add files via upload

---
+++ tp2 cinefilos (terminado)/carrousel.js
@@ -0,0 +1,32 @@
+const containers = document.querySelectorAll('.container, .articulos, .finalseccion');
+
+containers.forEach(container => {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    container.addEventListener('mousedown', (e) => {
+        isDown = true;
+        container.classList.add('active');
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+    });
+
+    container.addEventListener('mouseleave', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mouseup', () => {
+        isDown = false;
+        container.classList.remove('active');
+    });
+
+    container.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - container.offsetLeft;
+        const walk = (x - startX) * 1;
+        container.scrollLeft = scrollLeft - walk;
+    });
+});
